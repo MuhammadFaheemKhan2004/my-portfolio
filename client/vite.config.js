a3bf2bb9cd6f5091121dd4003f4ba0af919741a7
MuhammadFaheemKhan2004 <faheemniazi2004@gmail.com>
2026-04-03 01:35:00 +0500

    fix vite base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,15 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
+    base: '/my-portfolio/',
     build: {
+        sourcemap: false,
         outDir: 'build',
         emptyOutDir: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 })
